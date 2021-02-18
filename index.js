3fa37f1204f95597aed5e0cc4be56ccb8fec43c3
risaddex <risaddex@gmail.com>
2021-02-17 22:18:31 -0300

    All tests passing!

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //! CONSTS
-const width = 1280
-const height = window.screen.availHeight * .85
-const padding= 60;
+const WIDTH = 1280
+const HEIGHT = window.screen.availHeight * .85
+const PADDING = 60;
 
-const barHeight = Math.ceil(height - 2 * padding) / 12 ;
+const barHeight = Math.ceil(HEIGHT - 2 * PADDING) / 12 ;
+
+const legendWidth = WIDTH / 3;
+const legendHeight = HEIGHT * .1;
 
 
 
@@ -11,31 +14,34 @@ const barHeight = Math.ceil(height - 2 * padding) / 12 ;
 const svg =
   d3.select('.chart-container')
     .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT);
+    
 let dataset
 
 d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json',
   (e, json) => {
   dataset = json.monthlyVariance.map((item,i) => item);
 
-  const barWidth = (width - 2 * padding) / Math.ceil(dataset.length / 12);
-
+  const barWidth = (WIDTH - 2 * PADDING) / Math.ceil(dataset.length / 12);
+    const MAX = d3.max(dataset, d => Math.abs(d.variance))
   //! SCALES
-  const xScale =
+  const xScale = 
     d3.scaleLinear()
       .domain([d3.min(dataset, d => d.year), d3.max(dataset, d => d.year)])
-      .range([padding, width - padding]);
+      .range([PADDING, WIDTH - PADDING])
 
   const yScale =
     d3.scaleTime()
       .domain([new Date(2020, 0, 1), new Date(2020, 11, 30)])
-      .range([padding, height - padding]);
+      .range([PADDING, HEIGHT - PADDING]);
+  
   
   const colorScale = 
     d3.scaleLinear()
     .domain(d3.extent(dataset,d => d.variance))
     .range([1,0])
+
 
   // !AXIS FORMAT
   const xAxis =
@@ -50,26 +56,24 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
       .tickFormat(d3.timeFormat("%B"));
 
   // //! TOOLTIP
-  // const tooltip = d3.select('.chart-container')
-  //   .append('div')
-  //   .attr('id', 'tooltip')
-  //   .style('opacity', 0);
+  const tooltip = d3.select('.chart-container')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);
 
   //! AXIS CALLS
 
   const mainXaxis = svg
     .append('g')
-    .attr("transform", `translate(0,${height - padding})`)
+    .attr("transform", `translate(0,${HEIGHT - PADDING})`)
     .attr('id', 'x-axis')
     .call(xAxis);
 
   const mainYaxis = svg
     .append('g')
-    .attr("transform", `translate(${padding}, 0)`)
+    .attr("transform", `translate(${PADDING}, 0)`)
     .attr('id', 'y-axis')
     .call(yAxis)
-    .call(g => g.select(".domain")
-      .attr('length', 10))
 
 
   // !DOTS
@@ -78,7 +82,7 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
     .data(dataset)
     .enter()
     .append('rect')
-    .attr('x', (d) => xScale(d.year) - padding +1)
+    .attr('x', (d) => xScale(d.year) - PADDING +1)
     .attr('y', (d) => yScale(new Date(2020, d.month - 1)) -2)
     // .attr("transform", `translate(0,${ -  padding})`)
     .attr('width', barWidth)
@@ -88,40 +92,96 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
     .attr('data-month', (d) => d.month -1)
     .attr('data-year', (d) => d.year)
     .attr('data-temp', (d) => json.baseTemperature + d.variance)
+    .on('mouseover', (d) => {
+      let date = new Date(d.year, d.month -1, 12)
+      tooltip
+        .transition()
+        .duration(200)
+        .style('opacity', .9)
+        .attr('data-year', d.year)
+      tooltip
+        .html(`
+          <p>
+            <strong>Date</strong>: ${d3.timeFormat('%Y - %B')(date)}<br />
+            <strong>Temperature</strong>: ${d3.format('.1f')(json.baseTemperature + d.variance)}<br />
+            <strong>Variance</strong>: ${d3.format('+.1f')(d.variance)}<br />
+            ${
+              d.Doping
+                ? `<br />
+                  ${d.Doping}
+                `
+                : ''
+              }
+          </p>
+          
+        `)
+        .style('left', `${d3.event.screenX - PADDING}px`)
+        .style('top', `${d3.event.clientY - PADDING * 2}px`)
+    })
+    .on('mouseout', () => {
+      tooltip
+        .transition()
+        .duration(200)
+        .style('opacity', 0)
+    })
+
+  const legendScaleX =
+    d3.scaleLinear()
+      .domain(d3.extent(dataset, (d) => (d.variance + json.baseTemperature)))
+      .range([PADDING, legendWidth - PADDING]);
+
+  const thresHold =
+    d3.scaleThreshold()
+      .domain([].concat(
+          d3.schemeRdYlBu[11].map((item, i) => {
+            let interval = (legendScaleX.domain()[1] - legendScaleX.domain()[0]) / d3.schemeRdYlBu[11].length;
+            return legendScaleX.domain()[0] + i * interval;
+          })
+        )
+      )
+      .range(d3.schemeRdYlBu[11])
+  
+  const legendX =
+    d3.axisBottom()
+      .scale(legendScaleX)
+      .tickFormat(d3.format(""))
+      .tickValues(thresHold.domain())
+      .tickSize(20,0)
+      .tickFormat(d3.format('1.01f'))
+      
+
+  const legend = d3.select('#legend')
+    .append('svg')
+    .attr('height', legendHeight + PADDING )
+    .attr('width', legendWidth + PADDING)
+    .append('g')
+    .call(legendX)
+    
     
 
-
-  //   .on('mouseover', (d) => {
-  //     tooltip
-  //       .transition()
-  //       .duration(200)
-  //       .style('opacity', .9)
-  //       .attr('data-year', d.Year)
-  //     tooltip
-  //       .html(`
-  //         <p>
-  //           <strong>Name</strong>: ${d.Name}<br />
-  //           <strong>Time</strong>: ${d.Time}<br />
-  //           <strong>Year</strong>: ${d.Year}<br />
-  //           ${
-  //             d.Doping
-  //               ? `<br />
-  //                 ${d.Doping}
-  //               `
-  //               : ''
-  //             }
-  //         </p>
-          
-  //       `)
-  //       .style('left', `${d3.event.screenX - padding}px`)
-  //       .style('top', `${d3.event.clientY - padding * 2}px`)
-  //   })
-  //   .on('mouseout', () => {
-  //     tooltip
-  //       .transition()
-  //       .duration(200)
-  //       .style('opacity', 0)
-  //   })
+  legend
+    .selectAll('rect')
+    .data(thresHold.range().map((color) => {
+      let d = thresHold.invertExtent(color);
+      if (d[0] == null) {
+        d[0] = legendScaleX.domain()[0];
+      }
+      if (d[1] == null) {
+        d[1] = legendScaleX.domain()[1];
+      }
+      return d;
+    }))
+    .enter()
+    .insert("rect")
+    .attr('height', 20)
+    .attr('x', (d) => legendScaleX(d[0]))
+    .attr('width', (d) => legendScaleX(d[1]) - legendScaleX(d[0]))
+    .attr('fill', (d) => thresHold(d[0]))
+    .attr("transform", `translate(${15}, 0)`);
+  
+  legend
+    .select('g')
+    .attr("text-anchor", "middle")
 });
 
 
